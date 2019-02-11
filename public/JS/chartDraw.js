541abd454097f6215cc2b76323520b53406f5195
takahiromorita <moonchild194@gmail.com>
2019-02-11 17:34:48 +0900

    Update chartDraw.js

--- public/JS/chartDraw.js
+++ public/JS/chartDraw.js
@@ -4,10 +4,10 @@ function chartDraw(mychart){
         data: {
           labels: ["アンケート結果"],
           datasets: [
-            { label: "A", data: bar_List[0][1], backgroundColor: "rgba(244, 143, 177, 0.6)" },
-            { label: "B", data: bar_List[1][1], backgroundColor: "rgba(255, 235, 59, 0.6)" },
-            { label: "C", data: bar_List[2][1], backgroundColor: "rgba(100, 181, 246, 0.6)" },
-            { label: "D", data: bar_List[3][1], backgroundColor: "rgba(50, 81, 246, 0.6)" }
+            { label: "A", data: bar_List[0], backgroundColor: "rgba(244, 143, 177, 0.6)" },
+            { label: "B", data: bar_List[1], backgroundColor: "rgba(255, 235, 59, 0.6)" },
+            { label: "C", data: bar_List[2], backgroundColor: "rgba(100, 181, 246, 0.6)" },
+            { label: "D", data: bar_List[3], backgroundColor: "rgba(50, 81, 246, 0.6)" }
           ]
         },
 options: {
