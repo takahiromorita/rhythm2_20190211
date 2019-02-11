b001729b30e147a8d5c9a0bbff81cff20d638b0e
takahiromorita <moonchild194@gmail.com>
2019-02-11 18:24:21 +0900

    Update chartDraw.js

--- public/JS/chartDraw.js
+++ public/JS/chartDraw.js
@@ -7,7 +7,7 @@ function chartDraw(mychart){
             { label: "A", data: bar_List[0], backgroundColor: "rgba(244, 143, 177, 0.6)" },
             { label: "B", data: bar_List[1], backgroundColor: "rgba(255, 235, 59, 0.6)" },
             { label: "C", data: bar_List[2], backgroundColor: "rgba(100, 181, 246, 0.6)" },
-            { label: "D", data: bar_List[3], backgroundColor: "rgba(50, 81, 246, 0.6)" }
+            { label: myid, data: bar_List[3], backgroundColor: "rgba(50, 81, 246, 0.6)" }
           ]
         },
 options: {
